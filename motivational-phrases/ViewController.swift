2e65349e1613812d5acfb93db96ededb35036d60
Emerson Novais <emersonnovais93@gmail.com>
2022-03-19 00:05:09 -0300

    Motivational randomizer phrases
    Simple app to make a randomly motivational phrase

--- motivational-phrases/ViewController.swift
+++ motivational-phrases/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var phraseLabel: UILabel!
+    @IBAction func randomizerPhrase(_ sender: Any) {
+        var phrases: [String] = []
+        phrases.append("Não importa o que você decidiu. O que importa é que isso te faz feliz.")
+        phrases.append("Quando pensar em desistir, lembre-se porque começou.")
+        phrases.append("Se não puder fazer tudo, faça tudo que puder.")
+        phrases.append("Comece onde você está. Use o que você tem. Faça o que puder.")
+        
+        phraseLabel.text = phrases.randomElement()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
